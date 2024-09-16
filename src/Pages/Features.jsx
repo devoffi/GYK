63cc4b19ede9a00b8a5f18804bae10b07e072f25
Sankalp Dubey <sankalpdubey152002@gmail.com>
2024-09-17 00:27:17 +0530

    Features page

--- src/Pages/Features.jsx
+++ src/Pages/Features.jsx
@@ -1,9 +1,231 @@
-import React from 'react'
+import React from "react";
+import { SectionContainer } from "../components/common/SectionContainer";
+import PrimaryButton from "../components/common/PrimaryButton";
+import CreateAccount from "../components/CreateAccount";
 
 function Features() {
   return (
-    <div>Features</div>
-  )
+    <SectionContainer>
+      <p className="lg:w-1/2 text-center text-2xl lg:text-5xl font-bold mx-auto mt-20">
+        All the tools you need to protect your kids online
+      </p>
+      <p className="text-lg lg:text-xl text-center my-10">
+        Qustodio gives you the best parental control and digital wellbeing
+        features in one place
+      </p>
+      <div className="flex flex-wrap gap-10 lg:w-[80%] mx-auto">
+        <p className="px-4 py-2 border rounded-lg">Filter content & apps</p>
+        <p className="px-4 py-2 border rounded-lg">Monitor activity</p>
+        <p className="px-4 py-2 border rounded-lg">Set time limits</p>
+        <p className="px-4 py-2 border rounded-lg">Locate family</p>
+        <p className="px-4 py-2 border rounded-lg">Track calls & SMS</p>
+        <p className="px-4 py-2 border rounded-lg">Reports, alerts & SOS</p>
+      </div>
+
+      <div className="flex flex-col gap-20 lg:w-[80%] mx-auto">
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10">
+          <div>
+            <p className="text-xl lg:text-4xl font-bold mt-8">
+              Filter content & apps
+            </p>
+            <p className="text-xl lg:text-3xl font-bold mt-1 mb-8">
+              Create a safe space for your kids to explore and play online
+            </p>
+            <p className="text-lg">
+              Block inappropriate apps, games and websites. Allow your kids to
+              view child-friendly sites and automatically block potentially
+              harmful ones to prevent exposure to mature content, gambling,
+              violence and other threats. Receive alerts when they try to access
+              blocked content.
+            </p>
+          </div>
+          <div>
+            <video autoPlay muted loop type="webm">
+              <source src="https://static.qustodio.com/public-site/uploads/2024/04/26094824/supervise-the-way-you-want_EN.webm" />
+            </video>
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10">
+          <div>
+            <video autoPlay muted loop type="webm">
+              <source src="https://static.qustodio.com/public-site/uploads/2024/04/26094644/Keep-your-child-safe_EN.webm" />
+            </video>
+          </div>
+          <div>
+            <p className="text-xl lg:text-4xl font-bold mt-8">
+              Filter content & apps
+            </p>
+            <p className="text-xl lg:text-3xl font-bold mt-1 mb-8">
+              Create a safe space for your kids to explore and play online
+            </p>
+            <p className="text-lg">
+              Block inappropriate apps, games and websites. Allow your kids to
+              view child-friendly sites and automatically block potentially
+              harmful ones to prevent exposure to mature content, gambling,
+              violence and other threats. Receive alerts when they try to access
+              blocked content.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10">
+          <div>
+            <p className="text-xl lg:text-4xl font-bold mt-8">
+              Filter content & apps
+            </p>
+            <p className="text-xl lg:text-3xl font-bold mt-1 mb-8">
+              Create a safe space for your kids to explore and play online
+            </p>
+            <p className="text-lg">
+              Block inappropriate apps, games and websites. Allow your kids to
+              view child-friendly sites and automatically block potentially
+              harmful ones to prevent exposure to mature content, gambling,
+              violence and other threats. Receive alerts when they try to access
+              blocked content.
+            </p>
+          </div>
+          <div>
+            <video autoPlay muted loop type="webm">
+              <source src="https://static.qustodio.com/public-site/uploads/2024/04/26094824/supervise-the-way-you-want_EN.webm" />
+            </video>
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10">
+          <div>
+            <video autoPlay muted loop type="webm">
+              <source src="https://static.qustodio.com/public-site/uploads/2024/04/26094644/Keep-your-child-safe_EN.webm" />
+            </video>
+          </div>
+          <div>
+            <p className="text-xl lg:text-4xl font-bold mt-8">
+              Filter content & apps
+            </p>
+            <p className="text-xl lg:text-3xl font-bold mt-1 mb-8">
+              Create a safe space for your kids to explore and play online
+            </p>
+            <p className="text-lg">
+              Block inappropriate apps, games and websites. Allow your kids to
+              view child-friendly sites and automatically block potentially
+              harmful ones to prevent exposure to mature content, gambling,
+              violence and other threats. Receive alerts when they try to access
+              blocked content.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10">
+          <div>
+            <p className="text-xl lg:text-4xl font-bold mt-8">
+              Filter content & apps
+            </p>
+            <p className="text-xl lg:text-3xl font-bold mt-1 mb-8">
+              Create a safe space for your kids to explore and play online
+            </p>
+            <p className="text-lg">
+              Block inappropriate apps, games and websites. Allow your kids to
+              view child-friendly sites and automatically block potentially
+              harmful ones to prevent exposure to mature content, gambling,
+              violence and other threats. Receive alerts when they try to access
+              blocked content.
+            </p>
+          </div>
+          <div>
+            <video autoPlay muted loop type="webm">
+              <source src="https://static.qustodio.com/public-site/uploads/2024/04/26094824/supervise-the-way-you-want_EN.webm" />
+            </video>
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10">
+          <div>
+            <video autoPlay muted loop type="webm">
+              <source src="https://static.qustodio.com/public-site/uploads/2024/04/26094644/Keep-your-child-safe_EN.webm" />
+            </video>
+          </div>
+          <div>
+            <p className="text-xl lg:text-4xl font-bold mt-8">
+              Filter content & apps
+            </p>
+            <p className="text-xl lg:text-3xl font-bold mt-1 mb-8">
+              Create a safe space for your kids to explore and play online
+            </p>
+            <p className="text-lg">
+              Block inappropriate apps, games and websites. Allow your kids to
+              view child-friendly sites and automatically block potentially
+              harmful ones to prevent exposure to mature content, gambling,
+              violence and other threats. Receive alerts when they try to access
+              blocked content.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10 bg-green-50">
+          <div className="">
+            <img src="https://static.qustodio.com/public-site/uploads/2023/11/27112221/careplus-image-desktop%402x.png" />
+          </div>
+          <div className="lg:w-[80%]">
+            <p className="text-xl lg:text-4xl font-bold mt-8">
+              Make the most of Qustodio with Care Plus
+            </p>
+            <p className="text-lg lg:text-xl font-bold mt-1 mb-8">
+              Care Plus is our exclusive service designed to elevate your
+              Qustodio experience.{" "}
+            </p>
+            <p className="text-lg">
+              Block inappropriate apps, games and websites. Allow your kids to
+              view child-friendly sites and automatically block potentially
+              harmful ones to prevent exposure to mature content, gambling,
+              violence and other threats. Receive alerts when they try to access
+              blocked content.
+            </p>
+            <div className="my-5">
+              <PrimaryButton title={"Learn more"} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p className="text-center text-2xl lg:text-4xl font-bold mx-auto mt-20">
+        Qustodio works on every device you need
+      </p>
+      <p className="text-lg lg:text-xl text-center my-5">
+        Click on the icons below for more information on the features we provide
+        for each platform
+      </p>
+      <div className="flex flex-wrap gap-10 lg:w-[80%] mx-auto">
+        <p className="px-4 py-2 border rounded-lg"> Android</p>
+        <p className="px-4 py-2 border rounded-lg">iPhone & iPad</p>
+        <p className="px-4 py-2 border rounded-lg"> Windows</p>
+        <p className="px-4 py-2 border rounded-lg"> Mac</p>
+        <p className="px-4 py-2 border rounded-lg"> Chromebook</p>
+        <p className="px-4 py-2 border rounded-lg"> Kindle</p>
+      </div>
+
+      <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10 lg:w-[90%] lg:mx-auto">
+        <div className="">
+          <img src="https://static.qustodio.com/public-site/uploads/2023/11/27114336/windows-device%402x-1.png" />
+        </div>
+        <div className="lg:w-[80%]">
+          <p className="text-lg lg:text-2xl font-bold mt-8">
+            Qustodio for Chromebook
+          </p>
+          <p className="text-md lg:text-lg font-bold mt-1 mb-8">
+            For Chromebooks with Google Play (not available for non-Chromebook
+            devices running ChromeOS).
+          </p>
+          <p className="text-lg">
+            Web filtering & activity alerts Games & apps blocking + time limits
+            Limit social media use Daily time limits & restricted times Custom
+            routines Pause the internet Additional parent Youtube monitoring
+          </p>
+          <div className="my-5">
+            <PrimaryButton title={"Download"} />
+          </div>
+        </div>
+      </div>
+      <CreateAccount/>
+    </SectionContainer>
+  );
 }
 
-export default Features
+export default Features;
