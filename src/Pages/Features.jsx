7ce47ea3f6d494aca33168666b8a952a12fd696b
devbrat341 <devbratsingh341.com>
2024-10-02 11:17:07 +0530

    feature

--- src/Pages/Features.jsx
+++ src/Pages/Features.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { SectionContainer } from "../components/common/SectionContainer";
 import PrimaryButton from "../components/common/PrimaryButton";
 import CreateAccount from "../components/CreateAccount";
+import { LuCheckCircle } from "react-icons/lu";
 
 function Features() {
   return (
     <SectionContainer>
       <div className="text-gray-700">
-      <p className="md:w-3/5 text-center text-2xl lg:text-5xl font-bold mx-auto mt-20">
+      <p className="md:w-3/5 text-center text-2xl lg:text-5xl font-bold mx-auto mt-10 md:mt-20">
         All the tools you need to protect your kids online
       </p>
       <p className="text-lg lg:text-xl text-center my-10">
@@ -15,16 +16,28 @@ function Features() {
         features in one place
       </p>
       <div className="flex flex-wrap gap-6 lg:w-[90%] mx-auto">
-        <p className="px-4 py-2 border rounded-full">Filter content & apps</p>
-        <p className="px-4 py-2 border rounded-full">Monitor activity</p>
-        <p className="px-4 py-2 border rounded-full">Set time limits</p>
-        <p className="px-4 py-2 border rounded-full">Locate family</p>
-        <p className="px-4 py-2 border rounded-full">Track calls & SMS</p>
-        <p className="px-4 py-2 border rounded-full">Reports, alerts & SOS</p>
+        <div className="hidden md:flex md:flex-wrap gap-6 ">
+          <p className="px-4 py-2 border rounded-full">Filter content & apps</p>
+          <p className="px-4 py-2 border rounded-full">Monitor activity</p>
+          <p className="px-4 py-2 border rounded-full">Set time limits</p>
+          <p className="px-4 py-2 border rounded-full">Locate family</p>
+          <p className="px-4 py-2 border rounded-full">Track calls & SMS</p>
+          <p className="px-4 py-2 border rounded-full">Reports, alerts & SOS</p>
+        </div>
+        <div className="md:hidden flex overflow-x-auto scrollbar-hidden scrollbar-hide gap-2">
+          <p className="px-2 text-sm py-2 border rounded-full whitespace-nowrap">Filter content & apps</p>
+          <p className="px-2 text-sm py-2 border rounded-full whitespace-nowrap">Monitor activity</p>
+          <p className="px-2 text-sm py-2 border rounded-full whitespace-nowrap">Set time limits</p>
+          <p className="px-2 text-sm py-2 border rounded-full whitespace-nowrap">Locate family</p>
+          <p className="px-2 text-sm py-2 border rounded-full whitespace-nowrap">Track calls & SMS</p>
+          <p className="px-2 text-sm py-2 border rounded-full whitespace-nowrap">Reports, alerts & SOS</p>
+        </div>
       </div>
 
+
+
       <div className="flex flex-col gap-10 lg:w-full  mx-auto">
-        <div className="flex flex-col lg:flex-row bg-blue-50 rounded-xl p-16 my-10 justify-center items-center lg:gap-32 gap-10">
+        <div className="flex flex-col lg:flex-row bg-blue-50 rounded-xl p-4 md:p-16 my-10 justify-center items-center lg:gap-32 gap-10">
           <div>
             <p className="text-xl lg:text-4xl font-bold mt-8">
               Filter content & apps
@@ -69,7 +82,7 @@ function Features() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col lg:flex-row justify-center items-center bg-blue-50 rounded-xl p-16 my-10 lg:gap-32 gap-10">
+        <div className="flex flex-col lg:flex-row justify-center items-center bg-blue-50 rounded-xl p-4 md:p-16 my-10 lg:gap-32 gap-10">
           <div>
             <p className="text-xl lg:text-4xl font-bold mt-8">
               Filter content & apps
@@ -114,7 +127,7 @@ function Features() {
             </p>
           </div>
         </div>
-        <div className="flex flex-col lg:flex-row justify-center items-center bg-blue-50 rounded-xl p-16 my-10 lg:gap-32 gap-10">
+        <div className="flex flex-col lg:flex-row justify-center items-center bg-blue-50 rounded-xl p-4 md:p-16 my-10 lg:gap-32 gap-10">
           <div>
             <p className="text-xl lg:text-4xl font-bold mt-8">
               Filter content & apps
@@ -160,7 +173,7 @@ function Features() {
           </div>
         </div>
 
-        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10 rounded-xl py-16 my-10 bg-green-50">
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10 rounded-xl p-4 py-8 md:py-16 my-10 bg-green-50">
           <div className="">
             <img src="https://static.qustodio.com/public-site/uploads/2023/11/27112221/careplus-image-desktop%402x.png" />
           </div>
@@ -179,47 +192,77 @@ function Features() {
               violence and other threats. Receive alerts when they try to access
               blocked content.
             </p>
-            <div className="my-5">
+            <div className="my-5 flex justify-center">
               <PrimaryButton title={"Learn more"} />
             </div>
           </div>
         </div>
       </div>
 
-      <p className="text-center text-2xl lg:text-4xl font-bold mx-auto mt-20">
+      <p className="text-center text-2xl lg:text-4xl font-bold mx-auto mt-10 md:mt-20">
       GuardMyKids works on every device you need
       </p>
       <p className="text-lg lg:text-xl text-center my-5">
         Click on the icons below for more information on the features we provide
         for each platform
       </p>
-      <div className="flex flex-wrap gap-10 lg:w-[80%] mx-auto">
-        <p className="px-4 py-2 border rounded-lg"> Android</p>
-        <p className="px-4 py-2 border rounded-lg">iPhone & iPad</p>
-        <p className="px-4 py-2 border rounded-lg"> Windows</p>
-        <p className="px-4 py-2 border rounded-lg"> Mac</p>
-        <p className="px-4 py-2 border rounded-lg"> Chromebook</p>
-        <p className="px-4 py-2 border rounded-lg"> Kindle</p>
+      <div className="flex flex-wrap justify-center  gap-10 lg:w-[80%] mx-auto">
+        <p className="px-4 py-2 border-4 rounded-lg"> Android</p>
+        
       </div>
 
-      <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-32 gap-10 lg:w-[90%] lg:mx-auto">
+      <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-20 gap-10 lg:w-[90%] lg:mx-auto">
         <div className="">
-          <img src="https://static.qustodio.com/public-site/uploads/2023/11/27114336/windows-device%402x-1.png" />
+          <img src="https://static.qustodio.com/public-site/uploads/2023/11/27113514/ios-device%402x.png" />
         </div>
-        <div className="lg:w-[80%]">
-          <p className="text-lg lg:text-2xl font-bold mt-8">
-          GuardMyKids for Chromebook
+        <div className="lg:w-[80%] py-10 flex flex-col gap-1.5 ">
+          <p className="text-lg lg:text-2xl font-medium mt-8 tracking-[1px] md:tracking-[3px] ">
+          GuardMyKids for Android
           </p>
-          <p className="text-md lg:text-lg font-bold mt-1 mb-8">
-            For Chromebooks with Google Play (not available for non-Chromebook
-            devices running ChromeOS).
+          <p className="text-md lg:text-lg mt-1 mb-8  ">
+          For smartphones and tablets running Android 7.0 and up.
           </p>
-          <p className="text-lg">
-            Web filtering & activity alerts Games & apps blocking + time limits
-            Limit social media use Daily time limits & restricted times Custom
-            routines Pause the internet Additional parent Youtube monitoring
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Web filtering & activity alerts
           </p>
-          <div className="my-5">
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Games & apps blocking + time limits
+          </p>
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Limit social media use
+          </p>
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Daily time limits & restricted times
+          </p>
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Custom routines
+          </p>
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Pause the internet
+          </p>
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Additional parent
+          </p>
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Youtube monitoring
+          </p>
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Calls & SMS monitoring
+          </p>
+          <p className="text-lg flex">
+            < LuCheckCircle size={20} color={ "hsl(180, 98%, 31%)" } className="mt-1 mx-2 "  />
+            Location monitoring
+          </p>
+          <div className="my-5 md:pl-10 flex justify-center">
             <PrimaryButton title={"Download"} />
           </div>
         </div>
